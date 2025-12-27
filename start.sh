c4021d5e4dda855a3f9b71d5fbaf69c8534b2de1
RSG69 <69rsg69@gmail.com>
2025-12-27 11:36:21 +0100

    modificacion

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y unzip curl
 export PORT=${PORT:-3000}
-reflex run --env prod --backend-host 0.0.0.0 --single-port
+reflex run --env prod --backend-host 0.0.0.0 --single-port --port $PORT
